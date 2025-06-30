e6799a9fe34c482d5fc11e3e98613c075f30448f
triet-bit <911huynhminhtriet2019@gmail.com>
2025-06-30 14:28:05 +0700

    updating model, main, require

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from dotenv import load_dotenv
+import streamlit as st
 from graph.workflow import app
 from agent.state import AgentState
 from rag.vectordb import initialize_or_get_db
 from rag.embedding import create_sentence_embedding
 
+
 load_dotenv()
 
-if __name__ == "__main__":
+def init_db():
+    # Khởi tạo retriever config
+    embedding = create_sentence_embedding()
+    milvus_client = initialize_or_get_db(
+        db_path="vector_db/vector_db",
+        collection_name="docs",
+        docs=[],  # nếu collection đã tồn tại thì bỏ trống docs cũng được
+        embedding_model=embedding,
+        dimension=embedding.get_sentence_embedding_dimension()
+    )
+    return milvus_client
+def setup_page(): 
+    st.set_page_config(page_title="RAG Chatbot", layout="centered")
+    st.title("📚 RAG Chatbot: Ask Your PDFs")
+
+def main(): 
+   
+
     input_state = {
         "question": "Show me top 10 customers by sales",
         "sql_query": "",
@@ -19,15 +38,7 @@ if __name__ == "__main__":
 
     state = AgentState(**input_state)
 
-    # Khởi tạo retriever config
-    embedding = create_sentence_embedding()
-    milvus_client = initialize_or_get_db(
-        db_path="vector_db/vector_db",
-        collection_name="docs",
-        docs=[],  # nếu collection đã tồn tại thì bỏ trống docs cũng được
-        embedding_model=embedding,
-        dimension=embedding.get_sentence_embedding_dimension()
-    )
+    milvus_client = init_db()
 
     config = {
         "configurable": {
@@ -40,3 +51,8 @@ if __name__ == "__main__":
         for key, value in output.items():
             print(f"Node '{key}':")
             print(value["question"], value["sql_query"], value["query_result"], sep="\n")
+
+
+
+if __name__ == "__main__":
+    main()
